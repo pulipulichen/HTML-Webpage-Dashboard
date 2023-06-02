5bc43699d1feca3803793f3c9d7ab93d927e15e6
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-06-03 00:22:52 +0800

    Sat Jun  3 12:22:52 AM CST 2023

--- src/components/Dashboard/PanelButtonList/PanelButton/PanelButton.js
+++ src/components/Dashboard/PanelButtonList/PanelButton/PanelButton.js
@@ -87,14 +87,24 @@ let app = {
         window.IFRAME = this.$parent.$parent.$refs.PanelIframe
         console.log(this.$parent.$parent.$refs.PanelIframe)
         console.log(this.$parent.$parent.$refs.PanelIframe[id].url)
-        this.$parent.$parent.$refs.PanelIframe[id].url = url
+        // this.$parent.$parent.$refs.PanelIframe[id].url = url
         console.log(this.$parent.$parent.$refs.PanelIframe[id].url)
 
         console.log(this.config.dashboardConfig.tabs)
         console.log(this.localConfig.tab)
 
-        this.config.dashboardConfig.tabs.filter(t => (t.title === this.localConfig.tab))[0].url[id] = url
-        console.log('20230602-1619 ok')
+        for (let i = 0; i < this.config.dashboardConfig.tabs.length; i++) {
+          let {title} = this.config.dashboardConfig.tabs[i]
+
+          if (title !== this.localConfig.tab) {
+            continue
+          }
+
+          this.config.dashboardConfig.tabs[i].url[id] = url
+          break
+        }
+        // this.config.dashboardConfig.tabs.filter(t => (t.title === this.localConfig.tab))[0].url[id] = url
+        console.log('20230603-0020 okk')
       }
       else {
         this.utils.PopupUtils.openURLFullscreen(this.url)
